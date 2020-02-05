53ba4851deca3d8475ba98c9e4272928e59eecba
Manuel Cepeda <mecm1993@gmail.com>
2020-02-04 22:24:28 -0400

    chore: update documentation and refactor script
     - Paraphrase and update README with missing info
     - Refactor script due to consistency and minor change in text message

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,37 @@
-"use strict"
+'use strict'
 
 Swal.fire({
     title: 'Toxicity detection',
-    text: 'This work is maked for detect de toxicity of conversations or phrases. You can contant me via twitter @eddy_decena, telegram @eddy_decena, email eddynelson02@gmail.com',
+    text: 'The app is made to detect the toxicity of a converstion or phrases. You can reach me through, Twitter @eddy_decena, Telegram @eddy_decena, Email eddynelson02@gmail.com',
     icon: 'info',
     confirmButtonText: 'OK'
 })
 
-// get elements
+// Get elements
 const recording_button = document.getElementById('recording_button')
 const recording_icon = document.getElementById('recording_icon')
 const text_show = document.getElementById('voice-to-text-show')
 const toxicity_result = document.getElementById('toxicity_result')
 
-// global variable
-let recording = false
+// Global variables
 const threshold = 0.9
-let final_result = ""
+let recording = false
+let final_result = ''
 let label_results = [
-    "Identity attack",
-    "Insult",
-    "Obscene",
-    "Severe toxicity",
-    "Sexual explicit",
-    "Threat",
-    "Toxicity"
+    'Identity attack',
+    'Insult',
+    'Obscene',
+    'Severe toxicity',
+    'Sexual explicit',
+    'Threat',
+    'Toxicity'
 ]
 
 // Web speech api instance
 const recognition = new webkitSpeechRecognition()
 recognition.continuous = true
 recognition.interimResults = false
-recognition.lang = "en-US"
+recognition.lang = 'en-US'
 
 recognition.onresult = function(event) {
     let final_transcript = ''
@@ -49,22 +49,22 @@ recognition.onresult = function(event) {
         model.classify([final_transcript]).then(predictions => {
             predictions.forEach((pred, index) => {
                 if(pred.results[0].match === null || pred.results[0].match === true){
-                    final_result = final_result === "" ? 
+                    final_result = final_result === '' ? 
                                     `${label_results[index]}`
                                     :`${final_result}, ${label_results[index]}`
                 }
             })
     
-            toxicity_result.innerHTML = final_result === "" ? 
-                                        "It has not toxicity"
+            toxicity_result.innerHTML = final_result === '' ? 
+                                        'It has not toxicity'
                                         : final_result
                                         
-            final_result = ""
+            final_result = ''
         })
     })
 }
 
-// handle events
+// Handle events
 recording_button.addEventListener('click', () => {
     recording = !recording
     toggle_recording(
@@ -78,10 +78,10 @@ recording_button.addEventListener('click', () => {
 })
 
 // Functions
-function toggle_recording(recording_button, recording_icon, recognition, recording){
+function toggle_recording(recording_button, recording_icon, recognition, recording) {
     recording_button.classList.toggle('recording')
 
-    if(recording){
+    if(recording) {
         recording_icon.src = './assets/microphone-recording.svg'
         recognition.start()
         return
@@ -91,7 +91,7 @@ function toggle_recording(recording_button, recording_icon, recognition, recordi
     recognition.stop()
 }
 
-function toggle_start_opacity(el){
+function toggle_start_opacity(el) {
     el.classList.toggle('start_opacity')
 }
     
